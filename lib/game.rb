d2c1a2fdcfc2b76ef36813952f31ca96a2faa866
Robi <robi.maliqi@hotmail.co.uk>
2022-09-03 13:25:42 +0100

    adjusted code so draw tests pass

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,8 @@ class RockPaperScissors
       player1 == "scissors" && player2 == "paper" || 
       player1 == "paper" && player2 == "rock")
       "Player 1 wins"
+    elsif(player1 == player2)
+      "Draw"
    else
     "Player 2 wins"
    end
