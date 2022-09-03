6a7a6a7a89c3fa02551217442c9e5c7d4a38b229
Robi <robi.maliqi@hotmail.co.uk>
2022-09-03 12:55:01 +0100

    all tests pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,8 @@
 class RockPaperScissors
   def play(player1, player2)
     if (player1 == "rock" && player2 == "scissors" ||
-      player1 == "scissors" && player2 == "paper")
+      player1 == "scissors" && player2 == "paper" || 
+      player1 == "paper" && player2 == "rock")
       "Player 1 wins"
    else
     "Player 2 wins"
